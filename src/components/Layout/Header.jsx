63478f14dedeb142852706d36716cb3a9660bb48
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2022-09-17 13:17:17 +0530

    :lipstick: Add Header Button Component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
+
 import mealsImage from "../../assets/meals.jpg";
 import styles from "./Header.module.css";
 
@@ -8,7 +10,7 @@ const Header = (props) => {
         <React.Fragment>
             <header className={styles.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles["main-image"]}>
                 <img src={mealsImage} alt="A table full of delicious food!" />
